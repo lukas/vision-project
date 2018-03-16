d03f3c1b8b40d631e5bd388839f81aea0e8ab9c5
lukas <lukeab@gmail.com>
2018-03-15 22:45:08 -0700

    bugfix

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -1,15 +1,3 @@
-from keras.layers import Activation, Convolution2D, Dropout, Conv2D
-from keras.layers import AveragePooling2D, BatchNormalization
-from keras.layers import GlobalAveragePooling2D
-from keras.models import Sequential
-from keras.layers import Flatten
-from keras.models import Model
-from keras.layers import Input
-from keras.layers import MaxPooling2D
-from keras.layers import SeparableConv2D
-from keras import layers
-from keras.regularizers import l2
-
 from keras.layers import Dense, Flatten
 from keras.models import Sequential
 import pandas as pd
@@ -26,48 +14,7 @@ config.num_epochs = 10000
 input_shape = (64, 64, 1)
 
 wandb_callback=  WandbKerasCallback(save_model=False)
-def simple_CNN(input_shape, num_classes):
 
-    model = Sequential()
-    model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same',
-                            name='image_array', input_shape=input_shape))
-    model.add(BatchNormalization())
-    model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(.5))
-
-    model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(.5))
-
-    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(.5))
-
-    model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
-    model.add(Dropout(.5))
-
-    model.add(Convolution2D(filters=256, kernel_size=(3, 3), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Convolution2D(filters=num_classes, kernel_size=(3, 3), padding='same'))
-    model.add(GlobalAveragePooling2D())
-    model.add(Activation('softmax',name='predictions'))
-    return model
 
 def load_fer2013():
     
@@ -96,20 +43,15 @@ def load_fer2013():
 
 train_faces, train_emotions, val_faces, val_emotions = load_fer2013()
 num_samples, num_classes = train_emotions.shape
-train_faces /= 255.
-print(train_faces[0])
 
 model = Sequential()
 model.add(Flatten(input_shape=(48,48,1)))
-model.add(Dense(num_classes))
-model = simple_CNN((48,48,1), num_classes)
+model.add(Dense(num_classes, activation="softmax"))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy',
 metrics=['accuracy'])
 
-
-
-model.fit(train_faces[:1000], train_emotions[:1000], batch_size=config.batch_size,
+model.fit(train_faces, train_emotions, batch_size=config.batch_size,
                     epochs=config.num_epochs, verbose=1, callbacks=[wandb_callback],
                     validation_data=(val_faces, val_emotions))
 
